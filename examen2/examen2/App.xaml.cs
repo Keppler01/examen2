b211907ef52b7e4fd7665e05dd2a556d09fad06d
TheBe <TheBe@DESKTOP-PFHPH9I>
2021-11-19 01:33:26 -0600

    Agregar archivos de proyecto.

---
+++ examen2/examen2/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Repaso_Mysql_Xamarin
+{
+    public partial class App : Application
+    {
+        public static string url;
+        public App()
+        {
+            InitializeComponent();
+            url = "https://examenpm2.000webhostapp.com/ProyectoMySql/";
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ examen2/examen2/Clases/alumnosM.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace examen2
+{
+    class alumnosM
+    {
+        private HttpClient getCliente()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            client.DefaultRequestHeaders.Add("Connection", "close");
+            return client;
+        }
+        public async Task<IEnumerable<alumnos>> getUsuarios()
+        {
+            HttpClient client = getCliente();
+            var res = await client.GetAsync(App.url + "verUser.php");
+            if (res.IsSuccessStatusCode)
+            {
+                string content = await res.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<IEnumerable<alumnos>>(content);
+            }
+            return Enumerable.Empty<alumnos>();
+        }
+
+        public async Task<IEnumerable<alumnos>> TraerEstudiantes(string rne)
+        {
+            HttpClient client = getCliente();
+            var res = await client.GetAsync(App.url + "verUser.php?rne=" + rne);
+            if (res.IsSuccessStatusCode)
+            {
+                string content = await res.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<IEnumerable<alumnos>>(content);
+            }
+            return Enumerable.Empty<alumnos>();
+        }
+    }
+}
